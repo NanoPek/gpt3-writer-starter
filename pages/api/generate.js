ba83f7b452a570d49794cd57d091e781f3dcc5a2
Jérémie <jeremcarrez@gmail.com>
2022-11-28 23:55:24 +0100

    tt

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+const generateAction = async (req, res) => {
+    const basePromptPrefix = `Write me an easy and detailed step-by-step cooking recipe with a title with some of the following ingredients :\n${req.body.userInput}`;
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-002',
+        prompt: `${basePromptPrefix}${req.body.userInput}`,
+        temperature: 0.7,
+        max_tokens: 250,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
