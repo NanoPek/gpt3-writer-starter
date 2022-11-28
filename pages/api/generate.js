4d7aa1b0f9d0f1d1279574f9c51b87f4f9bd8d5d
Jérémie <jeremcarrez@gmail.com>
2022-11-29 00:38:08 +0100

    new gpt 3

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,7 +13,7 @@ const generateAction = async (req, res) => {
     console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
     const baseCompletion = await openai.createCompletion({
-        model: 'text-davinci-002',
+        model: 'text-davinci-003',
         prompt: `${basePromptPrefix}${req.body.userInput}`,
         temperature: 0.7,
         max_tokens: 250,
